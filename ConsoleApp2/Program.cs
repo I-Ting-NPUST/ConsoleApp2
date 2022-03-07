a19c47f8595951d4bb5047e44c91b2fba957f912
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:58:40 +0800

    a

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,8 +5,17 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            double money=0;
+            money = meal_cost+tip_percent + tax_percent;
+            Console.WriteLine("meal_cost 100 元");
+            Console.WriteLine("tip_percent 15元");
+            Console.WriteLine("tax_percent 8元");
+
+            Console.WriteLine("total "+money+"元");
         }
     }
 }
